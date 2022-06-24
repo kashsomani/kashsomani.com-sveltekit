c52715ed6c216292726ab955f73d793b9c810518
Kash Somani <kash@kashsomani.com>
2022-06-23 17:55:00 -0700

    added hero

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,12 @@
-<div>Hello!
-    I'm Kash Somani
-    and I am a Web Developer
-    (basically hackerman)</div>
+<div class="hero h-full bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Hello there</h1>
+      <h1 class="text-5xl font-bold">I'm Kash Somani</h1>
+      <p class="py-6">
+        and I am a Web Developer <span class="text-primary">|</span> Cloud Engineer <span class="text-primary">|</span> Statistician 
+      </p>
+      <button class="btn bg-base-100">Get Started</button>
+    </div>
+  </div>
+</div>
