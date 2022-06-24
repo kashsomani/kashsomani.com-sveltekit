7ca086dafaac9c8044dac0bdcb5b429bf4d098ca
Kash Somani <kash@kashsomani.com>
2022-06-23 17:58:39 -0700

    added portfolio route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,9 +4,9 @@
       <h1 class="text-5xl font-bold">Hello there</h1>
       <h1 class="text-5xl font-bold">I'm Kash Somani</h1>
       <p class="py-6">
-        and I am a Web Developer <span class="text-primary">|</span> Cloud Engineer <span class="text-primary">|</span> Statistician 
+        I am a Web Developer <span class="text-primary">|</span> Cloud Engineer <span class="text-primary">|</span> Statistician 
       </p>
-      <button class="btn bg-base-100">Get Started</button>
+      <button class="btn bg-base-100">Open Portfolio</button>
     </div>
   </div>
 </div>
