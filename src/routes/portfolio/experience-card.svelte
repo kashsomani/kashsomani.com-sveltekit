2323fa91beaf51ba02b650a920d653bad32c8f77
Kash Somani <kash@kashsomani.com>
2022-06-24 12:11:54 -0700

    card with tabs logic

--- src/routes/portfolio/experience-card.svelte
+++ src/routes/portfolio/experience-card.svelte
@@ -1,16 +1,70 @@
-<div class="grid grid-cols-1 place-items-center w-60 m-4 bg-base-300 rounded-2xl">
+<script>
+    let tabs = [
+        {
+            index: 0,
+            title: "1",
+        },
+        {
+            index: 1,
+            title: "1",
+        },
+        {
+            index: 2,
+            title: "1",
+        },
+    ];
+
+    let cards = [
+        {
+            index:0,
+            title: "experience 1",
+            text: ["hi"]
+        },
+        {
+            index:1,
+            title: "experience 2",
+            text: ["hello"]
+        },
+        {
+            index:2,
+            title: "experience 3",
+            text: ["greetings"]
+        }
+    ];
+    let currentlyActive = 0;
+</script>
+
+<div
+    class="grid grid-cols-1 place-items-center w-60 m-4 bg-base-300 rounded-2xl"
+>
     <div class="tabs min-w-max tab-border">
-        <a class="tab tab-bordered tab-active">Normal</a> 
-        <a class="tab tab-bordered">Normal</a> 
-        <a class="tab tab-bordered">Normal</a>
+        {#each tabs as tab}
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a
+                class="tab tab-bordered {tab.index === currentlyActive
+                    ? 'tab-active'
+                    : ''}"
+                on:click={() => {
+                    currentlyActive = tab.index;
+                }}>Normal</a
+            >
+        {/each}
     </div>
-    <div class="card bg-base-100 shadow-xl max-w-full rounded-t-none">
-        <div class="card-body max-w-prose">
-          <h2 class="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-sm btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+    {#each cards as card}
+       {#if card.index === currentlyActive}
+       <div
+       class="card bg-base-100 shadow-xl max-w-full min-w-full rounded-t-none"
+   >
+       <div class="card-body max-w-prose">
+           <h2 class="card-title">{card.title}</h2>
+           {#each card.text as p}
+               <p>{p}</p>
+           {/each}
+           <!-- <div class="card-actions justify-end">
+           <button class="btn btn-sm btn-primary">Buy Now</button>
+           </div> -->
+       </div>
+   </div>
+       {/if}
+    {/each}
 </div>
