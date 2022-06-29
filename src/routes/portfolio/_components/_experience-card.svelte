821f6b81063102efa309bb508f03e113383d004a
Kash Somani <kash@kashsomani.com>
2022-06-28 18:50:46 -0700

    added titles for card nav

--- src/routes/portfolio/_components/_experience-card.svelte
+++ src/routes/portfolio/_components/_experience-card.svelte
@@ -1,28 +1,31 @@
 <script>
-    import {fly} from "svelte/transition"
+    import { fly } from "svelte/transition";
     export let tabs = [
         {
             index: 0,
-            title: "1",
+            title: "General",
         },
         {
             index: 1,
-            title: "1",
+            title: "Tech",
         },
         {
             index: 2,
-            title: "1",
+            title: "Soft-Skills",
         },
     ];
     export let cards;
- let currentlyActive = 0;
+    let currentlyActive = 0;
 </script>
 
 <div
     class="flex flex-col place-items-stretch gap-0 max-w-1/3 mt-4 bg-base-300 rounded-2xl"
-    in:fly={{ x: 200, duration: 4000, delay: 3000}}
+    in:fly={{ x: 200, duration: 4000, delay: 3000 }}
 >
-    <div class="tabs tab-border m-0 p-0 grid grid-cols-3 place-items-stretch gap-0" id="tabs">
+    <div
+        class="tabs tab-border m-0 p-0 grid grid-cols-3 place-items-stretch gap-0"
+        id="tabs"
+    >
         {#each tabs as tab}
             <!-- svelte-ignore a11y-missing-attribute -->
             <a
@@ -31,36 +34,36 @@
                     : ''}"
                 on:click={() => {
                     currentlyActive = tab.index;
-                }}>Normal</a
+                }}>{tab.title}</a
             >
         {/each}
     </div>
     <div id="cards">
         {#each cards as card}
-        {#if card.index === currentlyActive}
-            <div
-                class="card bg-base-100 shadow-xl max-w-full min-w-full rounded-t-none h-full"
-            >
-                <div class="card-body max-w-prose">
-                    <h2 class="card-title">{card.title}</h2>
-                    {#each card.text as p}
-                        <p>{p}</p>
-                    {/each}
+            {#if card.index === currentlyActive}
+                <div
+                    class="card bg-base-100 shadow-xl max-w-full min-w-full rounded-t-none h-full"
+                >
+                    <div class="card-body max-w-prose">
+                        <h2 class="card-title">{card.title}</h2>
+                        {#each card.text as p}
+                            <p>{p}</p>
+                        {/each}
+                    </div>
                 </div>
-            </div>
-        {/if}
-    {/each}
+            {/if}
+        {/each}
     </div>
 </div>
 
 <style>
-    #cards{
+    #cards {
         width: 100%;
         height: 100%;
-        margin-top:0;
-        padding-top:0;
+        margin-top: 0;
+        padding-top: 0;
     }
-    a{
+    a {
         height: full;
     }
 </style>
