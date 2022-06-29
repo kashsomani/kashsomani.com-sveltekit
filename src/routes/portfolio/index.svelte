6af45b64d279543ea79cc7b82e0d940eb844c7a1
Kash Somani <kash@kashsomani.com>
2022-06-28 21:47:28 -0700

    fixed mobile bug for portfolio

--- src/routes/portfolio/index.svelte
+++ src/routes/portfolio/index.svelte
@@ -10,7 +10,7 @@
 
 <div class="w-full p-8 bg-base-200">
   <div
-    class="mockup-code border border-base-300 px-6 min-h-[20%] min-w-full max-w-full "
+    class="mockup-code border border-base-300 px:2 sm:px-6 min-h-[20%] min-w-full max-w-full text-sm sm:text-base "
   >
     {#if visible}
       <pre data-prefix="$" in:fly={{ y: 200, duration: 2000 }}><code
